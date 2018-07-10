02e09081b98fa552373f60f558814daf993c537d
Eric Zhang <ekzhang1@gmail.com>
2018-07-09 23:28:19 -0500

    Update treap implementation

--- treap.cpp
+++ treap.cpp
@@ -5,61 +5,51 @@
 using namespace std;
 
 struct tnode {
-	tnode* l;
-	tnode* r;
-	int x, y;
-	int s;
-	tnode() { y = rand() ^ (rand() << 16), s = 1; }
-	void update() { s = 1 + (l ? l->s : 0) + (r ? r->s : 0); }
+	tnode *l, *r;
+	int y = rand() ^ (rand() << 16);
+
+	/* Data */
+	int x;
+
+	/* Aggregate values */
+	int sz;
+
+	tnode() { update(); }
+	
+	void update() {
+		sz = 1 + (l ? l->sz : 0) + (r ? r->sz : 0);
+	}
+
+	void set(int x) { this->x = x; update(); }
+
 } vals[100013];
 int mem = 0;
 
 void split(tnode* t, tnode*& l, tnode*& r, int k) {
-	if (!t) {
-		l = r = nullptr;
-		return;
-	}
-	if (t->x < k) {
-		l = t;
-		split(t->r, t->r, r, k);
-	}
-	else {
-		r = t;
-		split(t->l, l, t->l, k);
-	}
+	if (!t) { l = r = nullptr; return; }
+	if (t->x < k)
+		l = t, split(t->r, t->r, r, k);
+	else
+		r = t, split(t->l, l, t->l, k);
 	t->update();
 }
 
 void split_at(tnode* t, tnode*& l, tnode*& r, int idx) {
-	if (!t) {
-		l = r = nullptr;
-		return;
-	}
-	int tidx = t->l ? t->l->s : 0;
-	if (tidx < idx) {
-		l = t;
-		split_at(t->r, t->r, r, idx - tidx - 1);
-	}
-	else {
-		r = t;
-		split_at(t->l, l, t->l, idx);
-	}
+	if (!t) { l = r = nullptr; return; }
+	int tidx = t->l ? t->l->sz : 0;
+	if (tidx < idx)
+		l = t, split_at(t->r, t->r, r, idx - tidx - 1);
+	else
+		r = t, split_at(t->l, l, t->l, idx);
 	t->update();
 }
 
 void merge(tnode*& t, tnode* l, tnode* r) {
-	if (!l || !r) {
-		t = l ? l : r;
-		return;
-	}
-	if (l->y < r->y) {
-		t = l;
-		merge(t->r, t->r, r);
-	}
-	else {
-		t = r;
-		merge(t->l, l, t->l);
-	}
+	if (!l || !r) { t = l ? l : r; return; }
+	if (l->y < r->y)
+		t = l, merge(t->r, t->r, r);
+	else
+		t = r, merge(t->l, l, t->l);
 	t->update();
 }
 
@@ -68,7 +58,7 @@ void insert(tnode*& t, int x) {
 	split(t, l, t, x);
 	split(t, t, r, x + 1);
 	t = &vals[mem++];
-	t->x = x;
+	t->set(x);
 	merge(t, l, t);
 	merge(t, t, r);
 }
@@ -77,13 +67,11 @@ void remove(tnode*& t, int x) {
 	tnode *l, *r;
 	split(t, l, t, x);
 	split(t, t, r, x + 1);
-	t = nullptr;
-	merge(t, l, t);
-	merge(t, t, r);
+	merge(t, l, r);
 }
 
 tnode* find_by_order(tnode* t, int k) {
-	int ls = t->l ? t->l->s : 0;
+	int ls = t->l ? t->l->sz : 0;
 	if (k < ls) return find_by_order(t->l, k);
 	if (k == ls) return t;
 	return find_by_order(t->r, k - 1 - ls);
@@ -136,15 +124,15 @@ int main() {
 	cout << endl;
 
 	cout << "Testing find_by_order" << endl;
-	for (int i = 0; i < l->s; i++)
+	for (int i = 0; i < l->sz; i++)
 		cout << "Order " << i << " in l: " << find_by_order(l, i)->x << endl;
 	cout << endl;
-	for (int i = 0; i < r->s; i++)
+	for (int i = 0; i < r->sz; i++)
 		cout << "Order " << i << " in r: " << find_by_order(r, i)->x << endl;
 	cout << endl;
 
 	merge(t, l, r);
-	
+
 	for (int i = 7; i <= 9; i++) {
 		insert(t, i);
 		cout << "After inserting " << i << endl;
